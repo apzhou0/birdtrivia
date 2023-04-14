879d81768230fd1975eaf605b2a48e4a91f4dd3d
zly00 <zlyaudrey00@gmail.com>
2023-04-13 22:38:35 -0700

    Generates new random bird tiles

--- birdbrawn/src/App.jsx
+++ birdbrawn/src/App.jsx
@@ -2,23 +2,28 @@ import React from 'react'
 import hum from './assets/images/hum.png'
 import './App.css'
 import BirdList from './components/BirdList.jsx'
-import Bird from './components/Bird.jsx'
-import { ReactSortable } from 'react-sortablejs';
 
 export default function App() {
   const [singleTile, setSingleTile] = React.useState([{id: 3, name: "singleBird", weight: "0", image: hum }]);
 
-  const [birds1, setBirds1] = React.useState([
+  const [birds, setBirds] = React.useState([
     { id: 1, name: "bird1", weight: "0.1", image: hum },
     { id: 2, name: "bird2", weight: "0.2", image: hum },
   ]);
+  
+  const newSingleTile = (event) => {
+    if (event.to.id === "birds") {
+      const newId = Math.floor(Math.random() * 1000);
+      setSingleTile([{ id: newId, name: "singlebird", weight: newId, image: hum }]);
+    }
+  };
 
   return (
     <div className="App">
       <div style={{ height: "400px" }}>
-      <BirdList birds={singleTile} setBirds={setSingleTile} group="shared"/>
+      <BirdList id="singleTile" birds={singleTile} setBirds={setSingleTile} group="shared" onEnd={newSingleTile} />
       </div>
-      <BirdList birds={birds1} setBirds={setBirds1} group="shared" />
+      <BirdList id="birds" birds={birds} setBirds={setBirds} group="shared"/>
     </div>
   );
 }
--- birdbrawn/src/components/BirdList.jsx
+++ birdbrawn/src/components/BirdList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { ReactSortable } from "react-sortablejs";
 import Bird from "./Bird"
 
-export default function BirdList ({birds, setBirds, group}) {
+export default function BirdList ({id, birds, setBirds, group, onEnd}) {
   return (
-    <ReactSortable list={birds} setList={setBirds} group={group}>
+    <ReactSortable id={id} list={birds} setList={setBirds} group={group} onEnd={onEnd} animation={100} delay={2} >
       {birds.map((bird) => (
         <Bird
           key={bird.id}
