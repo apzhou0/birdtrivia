44c466450f6326e5ad83e538a45bd091ef465607
zly00 <zlyaudrey00@gmail.com>
2023-04-17 13:47:29 -0700

    Changed design of tiles

--- birdbrawn/src/components/Bird.jsx
+++ birdbrawn/src/components/Bird.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 
 function Bird ({id, name, weight, image, rightSpot, isPlaced}) {
-  const tileStyle = {
-    backgroundImage: `url(${image})`,
-    backgroundSize: "contain",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    width: "240px",
-    height: "180px",
-  };
   const weightStyle = {
     fontSize: "20px",
     textAlign: "right",
@@ -16,18 +8,12 @@ function Bird ({id, name, weight, image, rightSpot, isPlaced}) {
     margin: "10px",
     textShadow: "0 0 7px black"
   };
-  const redDot = {
-    backgroundColor: "#cc0000"
-  };
-  const greenDot = {
-    backgroundColor: "#63ab57"
-  };
 
   return (
-    <div className="Bird--tile" style={tileStyle}>
-      <div className="Bird--dot" style={rightSpot ? greenDot : redDot}></div>
-      {isPlaced && <h2 className="Bird--weight" style={weightStyle}>{weight} g</h2>}
-      <h1 className = "Bird--name">{name}</h1>
+    <div className="Bird--tile">
+      <div className="Bird--weightbox">{weight}</div>
+      <img className="Bird--image" src={image}/>
+      <h1 className = "Bird--namebox">{name}</h1>
     </div>
   );
 };
